d608cf720e88791b4d3e7d2a5074fecfb3547c91
Peter Grzybowski <peter@northern.tech>
2020-12-04 15:03:19 +0100

    ProtoMsg moves in.
    ChangeLog:none
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- shell/exec.go
+++ shell/exec.go
@@ -20,24 +20,15 @@ import (
 	"sync"
 	"time"
 
-	"github.com/gorilla/websocket"
+	"github.com/mendersoftware/mender-shell/connection"
 	log "github.com/sirupsen/logrus"
-	"github.com/vmihailenco/msgpack"
+
+	"github.com/mendersoftware/go-lib-micro/ws"
+	wsshell "github.com/mendersoftware/go-lib-micro/ws/shell"
 )
 
 var (
-	ErrExecWriteBytesShort  = errors.New("failed to write the whole message")
-	MessageTypeShellCommand = "shell"
-	MessageTypeSpawnShell   = "new"
-	MessageTypeStopShell    = "stop"
-	writeWait               = 2 * time.Second
-)
-
-type MenderShellMessageStatus int
-
-const (
-	NormalMessage MenderShellMessageStatus = iota
-	ErrorMessage
+	ErrExecWriteBytesShort = errors.New("failed to write the whole message")
 )
 
 // MenderShellMessage represents a message between the device and the backend
@@ -48,7 +39,7 @@ type MenderShellMessage struct {
 	//message.
 	SessionId string `json:"session_id" msgpack:"session_id"`
 	//message status, currently normal and error message types are supported
-	Status MenderShellMessageStatus `json:"status_code" msgpack:"status_code"`
+	Status wsshell.MenderShellMessageStatus `json:"status_code" msgpack:"status_code"`
 	//the message payload, if
 	// * .Type===MessageTypeShellCommand interpreted as keystrokes and passed
 	//   to the stdin of the terminal running the shell.
@@ -60,7 +51,7 @@ type MenderShellMessage struct {
 type MenderShell struct {
 	writeMutex *sync.Mutex
 	sessionId  string
-	ws         *websocket.Conn
+	ws         *connection.Connection
 	r          io.Reader
 	w          io.Writer
 	running    bool
@@ -74,11 +65,11 @@ type MenderShellCommand struct {
 //are already connected to the i/o of the shell process and ws websocket
 //is connected and ws.SetReadDeadline(time.Now().Add(defaultPingWait))
 //was already called and ping-pong was established
-func NewMenderShell(sessionId string, writeMutex *sync.Mutex, ws *websocket.Conn, r io.Reader, w io.Writer) *MenderShell {
+func NewMenderShell(sessionId string, writeMutex *sync.Mutex, webSock *connection.Connection, r io.Reader, w io.Writer) *MenderShell {
 	shell := MenderShell{
 		writeMutex: writeMutex,
 		sessionId:  sessionId,
-		ws:         ws,
+		ws:         webSock,
 		r:          r,
 		w:          w,
 		running:    false,
@@ -86,8 +77,8 @@ func NewMenderShell(sessionId string, writeMutex *sync.Mutex, ws *websocket.Conn
 	return &shell
 }
 
-func MenderShellExecGetWriteTimeout() time.Duration {
-	return writeWait
+func (s *MenderShell) GetWriteTimeout() time.Duration {
+	return s.ws.GetWriteTimeout()
 }
 
 func (s *MenderShell) Start() {
@@ -103,8 +94,8 @@ func (s *MenderShell) IsRunning() bool {
 	return s.running
 }
 
-func (s *MenderShell) UpdateWSConnection(ws *websocket.Conn) error {
-	s.ws = ws
+func (s *MenderShell) UpdateWSConnection(webSock *connection.Connection) error {
+	s.ws = webSock
 	return nil
 }
 
@@ -124,19 +115,18 @@ func (s *MenderShell) pipeStdout() {
 			log.Errorf("error reading stdout: '%s'; restart is needed.", err)
 			break
 		}
-		m := &MenderShellMessage{
-			Type:      MessageTypeShellCommand,
-			Data:      raw[:n],
-			SessionId: s.sessionId,
+
+		msg := &ws.ProtoMsg{
+			Header: ws.ProtoHdr{
+				Proto:     ws.ProtoTypeShell,
+				MsgType:   wsshell.MessageTypeShellCommand,
+				SessionID: s.sessionId,
+				Properties: map[string]interface{}{
+					"status": wsshell.NormalMessage,
+				},
+			},
+			Body: raw[:n],
 		}
-		data, err := msgpack.Marshal(m)
-		if err != nil {
-			log.Errorf("error parsing message: %s", err)
-			continue
-		}
-		s.writeMutex.Lock()
-		s.ws.SetWriteDeadline(time.Now().Add(writeWait))
-		s.ws.WriteMessage(websocket.BinaryMessage, data)
-		s.writeMutex.Unlock()
+		err = s.ws.WriteMessage(msg)
 	}
 }
