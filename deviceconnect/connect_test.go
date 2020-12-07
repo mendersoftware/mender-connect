fb28b778a69b7240e33332462d8d64bc36dbf24a
Peter Grzybowski <peter@northern.tech>
2020-12-07 16:01:28 +0100

    MEN-4160 Server certificate: tests.
    ChangeLog:none
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- deviceconnect/connect_test.go
+++ deviceconnect/connect_test.go
@@ -15,8 +15,7 @@
 package deviceconnect
 
 import (
-	"github.com/mendersoftware/go-lib-micro/ws"
-	"github.com/vmihailenco/msgpack"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -25,6 +24,9 @@ import (
 
 	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
+	"github.com/vmihailenco/msgpack"
+
+	"github.com/mendersoftware/go-lib-micro/ws"
 )
 
 func TestGetWebSocketScheme(t *testing.T) {
@@ -93,7 +95,7 @@ func TestMenderShellDeviceConnect(t *testing.T) {
 	// Convert http://127.0.0.1 to ws://127.0.0.
 	u := "ws" + strings.TrimPrefix(server.URL, "http")
 
-	wsValid, err := Connect(u, "/", false, "token")
+	wsValid, err := Connect(u, "/", false, "", "token")
 	assert.Nil(t, err)
 	assert.NotNil(t, wsValid)
 	defer wsValid.Close()
@@ -109,12 +111,74 @@ func TestMenderShellDeviceConnect(t *testing.T) {
 	_, err = wsValid.ReadMessage()
 	assert.Error(t, err)
 
-	ws0, err := Connect("%2Casdads://:/sadfa//a", " same here", false, "token")
+	ws0, err := Connect("%2Casdads://:/sadfa//a", " same here", false, "", "token")
 	assert.Error(t, err)
 	assert.Nil(t, ws0)
 
 	t.Log("waiting for connection timeout")
-	ws1, err := Connect("wss://127.1.1.1:65534", "/this", false, "token")
+	ws1, err := Connect("wss://127.1.1.1:65534", "/this", false, "", "token")
 	assert.Error(t, err)
 	assert.Nil(t, ws1)
+}
+
+func TestMenderShellDeviceConnectTLSCerts(t *testing.T) {
+	testCases := map[string]struct {
+		certificate string
+		skipVerify  bool
+		noUpgrade   bool
+		err         error
+	}{
+		"ok-with-given-certificate": {
+			certificate: "testdata/server.crt",
+		},
+		"ok-without-verify": {
+			certificate: "",
+			skipVerify:  true,
+		},
+		"error-without-certificate": {
+			certificate: "",
+			err:         errors.New("x509: certificate signed by unknown authority"),
+		},
+		"error-bad-handshake": {
+			certificate: "testdata/server.crt",
+			noUpgrade:   true,
+			err:         errors.New("websocket: bad handshake"),
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if !tc.noUpgrade {
+					var upgrade = websocket.Upgrader{}
+					c, err := upgrade.Upgrade(w, r, nil)
+					if err != nil {
+						return
+					}
+					defer c.Close()
+
+					for {
+						c.SetWriteDeadline(time.Now().Add(2 * time.Second))
+						c.WriteMessage(websocket.BinaryMessage, []byte("echo"))
+
+						time.Sleep(4 * time.Second)
+					}
+				}
+			}))
+			defer server.Close()
+
+			// Convert http://127.0.0.1 to wss://127.0.0.
+			u := "wss" + strings.TrimPrefix(server.URL, "https")
+			wsValid, err := Connect(u, "/", tc.skipVerify, tc.certificate, "token")
+			if tc.err != nil {
+				assert.Error(t, err)
+				assert.EqualError(t, err, tc.err.Error())
+				assert.Nil(t, wsValid)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, wsValid)
+				defer wsValid.Close()
+			}
+		})
+	}
 }
