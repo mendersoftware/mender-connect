019c396f8ebf9ad2f8a347524353e9ac65802dc2
Lluis Campos <lluis.campos@northern.tech>
2021-12-20 16:57:09 +0100

    MEN-5290: Several robustness fixes around re-connection
    * Trigger re-connection when either token or server URL obtained from
      D-Bus signal has changed. Previously, it was enough with having a
      token with "some length" for the code to believe the device was
      authenticated and not trigger a re-connect.
    * Abort connecting after 10 retries. Previously it would reconnect
      forever with the same server URL, which will result in a deadlock if
      the url (or the token) changed while re-connecting.
    
    Changelog: Commit
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>
    (cherry picked from commit e3829373df9c6136c8eee1ead6b574776849dca0)
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- config/paths.go
+++ config/paths.go
@@ -38,7 +38,7 @@ var (
 
 	DefaultDebug = false
 
-	MaxReconnectAttempts             = uint(0) // 0 means to reconnect forever
+	MaxReconnectAttempts             = uint(10)
 	DefaultReconnectIntervalsSeconds = 5
 	MessageWriteTimeout              = 2 * time.Second
 	MaxShellsSpawned                 = uint(16)
